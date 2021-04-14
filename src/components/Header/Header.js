bf1499dee86626a3544c50826c8a69cc4ae67966
Kevin Spoth <kspoth08@su.edu>
2021-04-14 14:19:22 -0400

    adding header js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <h4>Sort and Search Through your Employee Database!</h4>
+    </div>
+  );
+}
+
+export default Header;
